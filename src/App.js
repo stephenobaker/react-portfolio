17054310a3376f01d68a17d81d75de07708e6e5d
System Administrator <stephengbaker@yahoo.com>
2019-07-21 22:44:32 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+class RadialWipeTop extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {scrollPosition: 0};
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    let newScroll = window.pageYOffset * -0.2;
+
+    this.setState({scrollPosition: newScroll});
+  }
+
+  render() {
+    return (
+      <div className="first-white-banner" style={{transform: `rotate(${this.state.scrollPosition}deg)`}} />
+    );
+  }
+}
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RadialWipeTop />
+      
     </div>
   );
 }
