8973c2c9d40436ed22e3d6eb34dd066bcbb5626f
Stephen Baker <stephengbaker@yahoo.com>
2019-08-09 13:39:43 -0500

    Add build files to tracking

---
+++ build/precache-manifest.c2f5c66c9b0bb4fd7d9295f94a269de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "bbf73b1a2761c2757b1f",
+    "url": "/react-portfolio/static/js/main.ab92379b.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/react-portfolio/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "bbf73b1a2761c2757b1f",
+    "url": "/react-portfolio/static/css/main.ea0e125c.chunk.css"
+  },
+  {
+    "revision": "bdbd81a11fc7b73df95b4d4afc8d2c21",
+    "url": "/react-portfolio/index.html"
+  }
+];
