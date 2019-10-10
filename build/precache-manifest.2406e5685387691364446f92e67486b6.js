edef24f3791c88d408044aa1abccd22e2b4516ce
Stephen Baker <stephengbaker@yahoo.com>
2019-10-10 18:56:37 -0500

    Initialize build

---
+++ build/precache-manifest.2406e5685387691364446f92e67486b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "36fed5eb0649deb60337",
+    "url": "/react-portfolio/static/js/main.13ad180d.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/react-portfolio/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "36fed5eb0649deb60337",
+    "url": "/react-portfolio/static/css/main.0b70a58c.chunk.css"
+  },
+  {
+    "revision": "cf2eadfad66d137094f24d861a289d03",
+    "url": "/react-portfolio/index.html"
+  }
+];
